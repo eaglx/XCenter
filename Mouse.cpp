daf6e39628bc0301639dfc2bb03dfa5c1d95f94c
eaglx <eaglx.programmer@gmail.com>
2019-12-03 10:16:30 +0100

    fix mouse module :shower:

--- Mouse.cpp
+++ Mouse.cpp
@@ -10,13 +10,19 @@ Mouse::~Mouse()
     if(display != NULL) XCloseDisplay(display);
 }
 
-bool Mouse::checkIfInit() 
+bool Mouse::checkIfInit()
 {
-    if(display == nullptr) return false;
+    if(display == nullptr)
+    {
+        return false;
+    }
+    s = DefaultScreenOfDisplay(display);
+    limitX = s->height;
+    limitY = s->width;
     return true;
 }
 
-void Mouse::move(int x, int y) 
+void Mouse::move(int x, int y)
 {
     XTestFakeRelativeMotionEvent(display, x, y, 0);
     XFlush(display);
@@ -35,20 +41,29 @@ void Mouse::button_break(unsigned int button)
 }
 
 
-void Mouse::setm(int x, int y) 
+void Mouse::setm(int x, int y)
 {
-    XTestFakeMotionEvent(display, 0, x, y, 0);
-    XFlush(display);
+    if((x >= limitX) || (y >= limitY) || (x <= 0) || (y <= 0))
+    {
+        ;
+    }
+    else
+    {
+        XTestFakeMotionEvent(display, 0, x, y, 0);
+        XFlush(display);
+    }
 }
 
-void Mouse::test() 
+void Mouse::test()
 {
     setm(0, 0);
     move(100, 100);
     button_make(RIGHT_MOUSE_BUTTON);
     button_break(RIGHT_MOUSE_BUTTON);
-
-    // Cannot test on the VBox
-    XWarpPointer(display, None, None, 0, 0, 0, 0, 10, 10); // Or use xdotool mousemove x y
-    XFlush(display);
+    std::cout << "Screen Height: " << s->height << " Width: " << s->width << std::endl;
+    // Cannot test on the VBox with integration drivers
+    // below moves the pointer by the offsets (dest_x, dest_y)
+    // relative to the current position of the pointer.
+    // XWarpPointer(display, None, None, 0, 0, 0, 0, 10, 10); // Or use xdotool mousemove x y
+    // XFlush(display);
 }
--- Mouse.hpp
+++ Mouse.hpp
@@ -3,8 +3,10 @@
 #include <X11/Xutil.h>
 #include <X11/extensions/XTest.h>
 
+#include <iostream>
+
 /*
-Ubuntu need to install: x11proto-dev, libxtst-dev, xdotool
+Ubuntu need to install: libxtst-dev, xdotool
 */
 
 #define LEFT_MOUSE_BUTTON 1
@@ -13,13 +15,16 @@ Ubuntu need to install: x11proto-dev, libxtst-dev, xdotool
 class Mouse{
     private:
         Display *display;
+        Screen *s;
+        int limitX;
+        int limitY;
     public:
         Mouse();
         ~Mouse();
         bool checkIfInit();
         void button_make(unsigned int);
-        /* 
-            button_*(value): 
+        /*
+            button_*(value):
             value = 1 - left mouse button
                     3 - right mouse button
         */
